5147bbb99dccb557c2e2ab2d14fab3e95af41c3f
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-05-23 19:06:48 +0300

    section loader added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 initCanvas();
 initBlast();
 initType();
+
+document.querySelectorAll('.list').forEach(elem=> {
+    elem.addEventListener('click', preloader)
+})
+
+function preloader() {
+    document.querySelectorAll('.section-loader').forEach(e => {
+        e.classList.add('active')
+        setTimeout(() => {
+            e.classList.remove('active')
+        }, 1700)
+    })
+    document.querySelectorAll('.section').forEach(e => {
+        e.classList.add('hiding')
+        setTimeout(() => {
+            e.classList.remove('hiding')
+        }, 1700)
+    })
+}

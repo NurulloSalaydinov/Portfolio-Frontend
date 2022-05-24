b5f94548ca661ae93e9104dece890096e61d842d
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-05-24 15:18:39 +0300

    preloader added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-import { initSection } from './section.js'
-import { initCanvas } from './canvas.js'
-import { initBlast } from './rubber.js'
-import { initType } from './initType.js'
-import { addActiveClass } from './navigation.js'
+import { initSection } from './section.js';
+import { initCanvas } from './canvas.js';
+import { initBlast } from './rubber.js';
+import { initType } from './initType.js';
+import { addActiveClass } from './navigation.js';
 
-addActiveClass(); // add active class to list of navigation
-initCanvas(); // Start Canvas
-initBlast(); // Start blast animation
-initType(); // Start Typing animation
-initSection(); // Activate Section
+window.addEventListener('DOMContentLoaded', () => {
+    addActiveClass(); // add active class to list of navigation
+    initCanvas(); // Start Canvas
+    initBlast(); // Start blast animation
+    initType(); // Start Typing animation
+    initSection(); // Activate Section
+    document.querySelector('.loader').style.opacity = '0';
+});

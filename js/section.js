41493ffe27cd6a4ca9df4b80c01b9f6b7c9bd588
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-06-02 23:22:25 +0300

    skills section finished

--- js/section.js
+++ js/section.js
@@ -1,21 +1,46 @@
 let initSection = () => {
 
-let lists = document.querySelectorAll('.list').forEach(listIterator);
-let sectionWrapper = document.querySelector('.section-wrapper');
+    let lists = document.querySelectorAll('.list').forEach(listIterator);
+    let sectionWrapper = document.querySelector('.section-wrapper');
 
-function listIterator(e, i) {
-    e.addEventListener('click', (event) => {
-        let totalMove;
-        if(i==0) {
-            console.log('bu 0 element')
-            sectionWrapper.style = 'transform: translateY(100vh)'
-        } else {
-            totalMove=(i*100-100)
-            sectionWrapper.style = `transform: translateY(-${totalMove}vh)`
-        }
-    })
-}
+    let circles = document.querySelectorAll('circle');
 
-}
+    function listIterator(e, i) {
+        e.addEventListener('click', (event) => {
+            let totalMove;
+            if (i == 0) {
+                sectionWrapper.style = 'transform: translateY(100vh)'
+            } else {
+                totalMove = (i * 100 - 100)
+                sectionWrapper.style = `transform: translateY(-${totalMove}vh)`
+            }
+            if (i == 2) {
+                setTimeout(() => {
+                    let progressBar = document.querySelectorAll(".circular-progress").forEach(e => {
+                        let progressValue = 0;
+                        let progressEndValue = parseInt(e.getAttribute("data-end-percent"));
+                        let speed = 15;
+        
+                        let progress = setInterval(() => {
+                            progressValue++;
+                            e.style.background = `conic-gradient(
+                                var(--navigation-color) ${progressValue * 3.6}deg,
+                                var(--gray) ${progressValue * 3.6}deg
+                            )`;
+                            if (progressValue == progressEndValue) {
+                                clearInterval(progress);
+                            }
+                        }, speed);
+                    });
+    
+                }, 950)
 
-export {initSection};
+            };
+        });
+    };
+
+};
+
+export {
+    initSection
+};

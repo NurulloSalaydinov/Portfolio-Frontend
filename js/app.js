3235adab57d9149d0b57280f097e737570cccaa3
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-06-17 10:25:49 +0300

    bootstrap removed for test

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 import { initSection } from './section.js';
-// import { initCanvas } from './canvas.js';
+import { initCanvas } from './canvas.js';
 import { initBlast } from './rubber.js';
 import { initType } from './initType.js';
 import { addActiveClass } from './navigation.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     addActiveClass(); // add active class to list of navigation
-    // initCanvas(); // Start Canvas
+    initCanvas(); // Start Canvas
     initBlast(); // Start blast animation
     initType(); // Start Typing animation
     initSection(); // Activate Section

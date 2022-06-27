b006b7cc9dd262dcd36ba683c1e77e1b5673d850
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-06-27 11:45:38 +0300

    blog added but not finished

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ import {
 import {
     addActiveClass
 } from './navigation.js';
+import {
+    activateSlider
+} from './slider.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     addActiveClass(); // add active class to list of navigation
@@ -20,6 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     initBlast(); // Start blast animation
     initType(); // Start Typing animation
     initSection(); // Activate Section
+    activateSlider('#sliderScroll'); // Activate Slider
     document.querySelector('.loader').style.opacity = '0';
     setTimeout(() => {
         document.querySelector('.loader').style.display = 'none';

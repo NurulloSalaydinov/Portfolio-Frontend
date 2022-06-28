6ba931c20fb3f7f2b55191718378864fc05d4e58
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-06-28 20:39:25 +0300

    updated slider js

--- js/slider.js
+++ js/slider.js
@@ -2,63 +2,22 @@ const activateSlider = (elem_attr) => {
     let elem = document.querySelector(elem_attr);
     let elem_items = [...elem.querySelectorAll('article')];
     let x = 0;
-    let scrollPos;
-    let clones = [];
-    let clonesWidth;
-    let sliderWidth;
-
-    elem_items.forEach(item => {
-        let clone = item.cloneNode(true);
-        clone.classList.add('clone');
-        elem.appendChild(clone);
-        clones.push(clone);
-    });
-
-    function getClonesWidth() {
-        let width = 0;
-        clones.forEach(clone => {
-            width += clone.offsetWidth;
-        })
-        return width
-    }
-
-    function getScrollPos() {
-        return window.scrollY;
-    }
-
-    function setScrollPos(pos) {
-        window.scrollTo({
-            top: pos
-        })
-    }
-
-    function scrollUpdate() {
-        scrollPos = getScrollPos();
-        if (clonesWidth + scrollPos >= sliderWidth) {
-            window.scrollTo({
-                top: 1
-            });
-        } else if (scrollPos <= 0) {
-            window.scrollTo({
-                top: sliderWidth - clonesWidth - 1
-            });
-        };
-
-        elem.style.tranform = `translateX(${-window.scrollY}px)`;
-        requestAnimationFrame(scrollUpdate);
-    };
-
-    function onLoad() {
-        document.body.style.height = `${sliderWidth}px`;
-    }
-
-    onLoad()
     // console.dir(elem);
     // console.log(elem_items);
-    // setInterval(() => {
-    // x += 10;
-    // elem.style = `transform: translateX(${x}px)`
-    // }, 100);
+    function startSlide() {
+        setInterval(() => {
+            x += 10;
+            elem.style = `transform: translateX(${x}px)`
+        }, 100);
+    }
+    elem.addEventListener('transitioned', () => {
+        elem.appendChild(elem.firstElementChild);
+        elem.style.transition = 'none';
+        elem.style.transform = 'translateX(0)';
+        setTimeout(() => {
+            elem.style.transition = 'all 0.5s';
+        })
+    })
 };
 
 export {

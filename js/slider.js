d66ab50aeb7056d82b17d9682cccfe04256b7507
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-06-28 23:19:03 +0300

    design updates
    bug appeared at js

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,13 @@
 const activateSlider = (elem_attr) => {
     let elem = document.querySelector(elem_attr);
-    let elem_items = [...elem.querySelectorAll('article')];
+    let cloned_elem = elem.cloneNode(true);
     let x = 0;
-    // console.dir(elem);
-    // console.log(elem_items);
-    function startSlide() {
-        setInterval(() => {
-            x += 10;
-            elem.style = `transform: translateX(${x}px)`
-        }, 100);
-    }
-    elem.addEventListener('transitioned', () => {
-        elem.appendChild(elem.firstElementChild);
-        elem.style.transition = 'none';
-        elem.style.transform = 'translateX(0)';
-        setTimeout(() => {
-            elem.style.transition = 'all 0.5s';
-        })
-    })
+    elem.classList.add('start-run');
+    setTimeout(() => {
+        elem.parentNode.prepend(cloned_elem);
+        elem.parentNode.removeChild(elem);
+    });
+    set
 };
 
 export {
